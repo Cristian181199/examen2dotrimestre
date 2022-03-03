a4b48c9874b8274b4306df26db7ad9514d83a4e0
Cristian Quintero Garcia <cristian181199@gmail.com>
2022-03-03 13:50:15 +0100

    Si la monografia tiene articuos, los muestra, si no tiene, le muestra al usuario un mensaje que dice que no tiene

--- resources/views/monographs/show.blade.php
+++ resources/views/monographs/show.blade.php
@@ -8,14 +8,19 @@
         <h3 class="border-t mb-2 pt-3 font-semibold">Title: <span class="font-thin">{{ $monographwitharticles[0]->title }}</span></p>
         <h3 class="border-t mb-2 pt-3 font-semibold">Year: <span class="font-thin">{{ $monographwitharticles[0]->year }}</span></p>
     </div>
-
-    <div class="bg-white rounded-md max-w-4xl mx-auto p-4 space-y-4 shadow-lg">
-        <p class=" mb-2 pt-3 font-semibold text-2xl">Articles that make up the monograph</p>
-        @foreach ($monographwitharticles[0]->articles as $article)
-            <h3 class="border-t mb-2 pt-3 font-semibold">Title of article: <span class="font-thin">{{ $article->title }}</span></p>
-        @endforeach
-        <h3 class="border-t mb-2 pt-3 font-semibold">Sum of the number of pages of all your articles: <span class="font-thin">{{ $monographwitharticles[0]->articles_sum_number_pages }}</span></p>
-    </div>
+    @if ($monographwitharticles[0]->articles->count() === 0)
+        <div>
+            <p class=" mb-2 pt-3 font-semibold text-2xl">This monograph has no articles.</p>
+        </div>
+    @else
+        <div class="bg-white rounded-md max-w-4xl mx-auto p-4 space-y-4 shadow-lg">
+            <p class=" mb-2 pt-3 font-semibold text-2xl">Articles that make up the monograph</p>
+            @foreach ($monographwitharticles[0]->articles as $article)
+                <h3 class="border-t mb-2 pt-3 font-semibold">Title of article: <span class="font-thin">{{ $article->title }}</span></p>
+            @endforeach
+            <h3 class="border-t mb-2 pt-3 font-semibold">Sum of the number of pages of all your articles: <span class="font-thin">{{ $monographwitharticles[0]->articles_sum_number_pages }}</span></p>
+        </div>
+    @endif
     <div class="flex justify-end space-x-2 pt-3">
         <a href="{{ route('monographs.index') }}" class="bg-red-500 hover:bg-red-400 transition-colors rounded-[8px] px-[15px] py-[4px] text-white focus:ring-2 ring-red-500">Return</a>
     </div>
